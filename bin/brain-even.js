4707d25a25da0a2f88d0edf6133b8b8479e22b05
dmitry.buyan <buyandl@mail.ru>
2022-06-27 22:43:39 +0400

    add even numbers check

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log('hello');
+import { greetUser, userName } from '../src/cli.js';
+import isAnswerCorrect from '../src/check-answer.js';
+import { getRandomInteger } from '../src/utils.js';
+
+greetUser();
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+let correctAnswers = 0;
+const maxCorrectAnswers = 3;
+
+while (correctAnswers < maxCorrectAnswers) {
+  if (isAnswerCorrect(getRandomInteger())) {
+    correctAnswers += 1;
+
+    if (correctAnswers === maxCorrectAnswers) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  } else {
+    break;
+  }
+}
