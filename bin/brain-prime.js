77b3bde94c1046d4967b15ba1ec33342de1719e6
dmitry.buyan <buyandl@mail.ru>
2022-10-04 23:11:17 +0400

    add new game

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import checkPrimeNumber from '../games/brain-prime.js';
+
+checkPrimeNumber();
---
+++ games/brain-prime.js
@@ -0,0 +1,69 @@
+import {
+  MAX_ROUNDS,
+  REPLY_POSITIVE,
+  REPLY_NEGATIVE,
+  welcomeUser,
+  getUserReply,
+  showUserReply,
+  getRandomInteger,
+  finishOnSuccess,
+  showSuccessMessage,
+  showErrorMessage,
+} from '../src/index.js';
+
+const intro = `Answer "${REPLY_POSITIVE}" if given number is prime. Otherwise answer "${REPLY_NEGATIVE}".`;
+const userName = welcomeUser(intro);
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const checkPrimeNumber = () => {
+  let isReplyCorrect = true;
+
+  const checkUserReply = (number) => {
+    const userReply = getUserReply(number);
+    showUserReply(userReply);
+
+    if (isPrime(number) && userReply === REPLY_POSITIVE) {
+      showSuccessMessage();
+    } else if (!isPrime(number) && userReply === REPLY_NEGATIVE) {
+      showSuccessMessage();
+    } else if (isPrime(number) && userReply === REPLY_NEGATIVE) {
+      showErrorMessage(userReply, REPLY_POSITIVE, userName);
+      isReplyCorrect = false;
+    } else {
+      showErrorMessage(userReply, REPLY_NEGATIVE, userName);
+      isReplyCorrect = false;
+    }
+  };
+
+  let totalCorrectAnswers = 0;
+
+  for (let i = 0; i < MAX_ROUNDS; i += 1) {
+    const randomInteger = getRandomInteger();
+    checkUserReply(randomInteger);
+
+    if (isReplyCorrect) {
+      totalCorrectAnswers += 1;
+    } else {
+      break;
+    }
+  }
+
+  if (totalCorrectAnswers === MAX_ROUNDS) {
+    finishOnSuccess(userName);
+  }
+};
+
+export default checkPrimeNumber;
