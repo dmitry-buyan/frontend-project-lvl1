16aff202738fc131bba641871ee00e119d3d1233
dmitry.buyan <buyandl@mail.ru>
2022-10-09 20:44:54 +0400

    refactor brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -16,7 +16,7 @@ const isEven = (number) => number % 2 === 0;
 const intro = `Answer "${REPLY_POSITIVE}" if the number is even, otherwise answer "${REPLY_NEGATIVE}".`;
 const userName = welcomeUser(intro);
 
-const checkReply = () => {
+const checkUserReply = () => {
   let isReplyCorrect = true;
   const num = getRandomInteger();
   const reply = getUserReply(num);
@@ -32,6 +32,6 @@ const checkReply = () => {
   return isReplyCorrect;
 };
 
-const runEvenGame = () => repeatTask(checkReply, userName, finishOnSuccess);
+const runEvenGame = () => repeatTask(checkUserReply, userName, finishOnSuccess);
 
 export default runEvenGame;
